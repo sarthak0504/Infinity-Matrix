9401d90fb24bd5e00a83f307990b6e05ee36aff2
harshjainh2j <141403634+harshjainh2j@users.noreply.github.com>
2024-09-18 13:50:26 +0530

    login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Login from './components/Login/Login.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -33,10 +34,12 @@ import User from './components/User/User.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
+    <Route path='' element={<Login/>}/>
+      <Route path='home' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      
  
     </Route>
   )
